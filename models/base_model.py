5f01471935b99568d90100cd2d229217ee4fcea0
middo1 <hamidadeshina1@gmail.com>
2022-09-13 19:00:56 +0100

    leggo

--- models/base_model.py
+++ models/base_model.py
@@ -79,9 +79,10 @@ class BaseModel:
         cp_dct['__class__'] = self.__class__.__name__
         cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        cp_dct.pop('_sa_instance_state', None)
+        if '_sa_instance_state' in cp_dct:
+            cp_dct.__delitem__('_sa_instance_state')
 
-        return (cp_dct)
+        return cp_dct
 
     def delete(self):
         """

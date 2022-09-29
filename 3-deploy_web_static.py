4b00c551fa469278002c728610318b73e5807722
middo1 <hamidadeshina1@gmail.com>
2022-09-29 11:05:44 +0100

    asin

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,8 +7,8 @@ from os import path
 
 
 env.hosts = ['34.239.158.227', '3.236.24.153']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+# env.user = 'ubuntu'
+# env.key_filename = '~/.ssh/school'
 
 
 
@@ -38,19 +38,19 @@ def do_deploy(archive_path):
             put(archive_path, '/tmp/')
             timestamp = archive_path[-18:-4]
             run('sudo mkdir -p /data/web_static/\
-                 releases/web_static_{}/'.format(timestamp))
+releases/web_static_{}/'.format(timestamp))
             run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-                 /data/web_static/releases/web_static_{}/'
+/data/web_static/releases/web_static_{}/'
                 .format(timestamp, timestamp))
             run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
             run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-                 /data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
             run('sudo rm -rf /data/web_static/releases/\
-                 web_static_{}/web_static'
+web_static_{}/web_static'
                 .format(timestamp))
             run('sudo rm -rf /data/web_static/current')
             run('sudo ln -s /data/web_static/releases/\
-                 web_static_{}/ /data/web_static/current'.format(timestamp))
+web_static_{}/ /data/web_static/current'.format(timestamp))
     except:
             return False
 
